2867894a8ea515b882a884f6a5cd0483e08279f3
javcastilla <javier.casno@gmail.com>
2026-06-10 17:04:27 +0100

    feat: migrated to synthea api example

--- src/main/java/software/examples/spool/boe/Application.java
+++ src/main/java/software/examples/spool/boe/Application.java
@@ -12,12 +12,12 @@ public class Application {
     public Application() throws IOException {
         this.runtime = SpoolRuntime.builder()
                 .OpenTelemetryConfiguration(OpenTelemetryConfiguration.builder()
-                    .serviceName("SyntheaPipeline")
+                    .serviceName("SyntheaExamplePipeline")
                     .logsEndpoint("http://localhost:3100/otlp/v1/logs")
                     .metricsEndpoint("http://localhost:4320/v1/metrics")
                     .tracesEndpoint("http://localhost:4318/v1/traces")
                     .build())
-                .withNode(SpoolNodeDSL.fromDescriptor("/pipeline-boe.yaml"))
+                .withNode(SpoolNodeDSL.fromDescriptor("/pipeline.yaml"))
                 .build();
     }
 
